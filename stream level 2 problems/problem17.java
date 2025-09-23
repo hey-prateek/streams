6bad6691f6c8b4a0146e5a935a2d151c7c0af8f2
hey-prateek <prateektiwari010799@gmail.com>
2025-09-23 17:29:15 +0530

    stream practice

---
+++ stream level 2 problems/problem17.java
@@ -0,0 +1,14 @@
+// How to convert a List of objects into a Map by considering duplicated keys and store them in
+// sorted order
+
+import java.util.Arrays;
+import java.util.List;
+
+public class problem17 {
+    public static void main(String[] args)
+    {
+        List<String> lst=Arrays.asList("hey", "prateek", "muskan", "hey", "muskan");
+
+        
+    }
+}
