9bba1d775fcc19df176cbe7405960381ef56de32
hey-prateek <prateektiwari010799@gmail.com>
2025-09-23 22:29:53 +0530

    merging stream practice codes

--- stream level 2 problems/getSumOfDuplicates.java
+++ stream level 2 problems/getSumOfDuplicates.java
@@ -12,6 +12,10 @@ public class getSumOfDuplicates {
         // or can use below
         // int res = lst.stream().filter(s -> Collections.frequency(lst, s) > 1).reduce(0, (a,b)-> a+b);
 
-        System.out.print(res);
+        System.out.println(res);
+
+
+        // int[] arr=new int[]{1,2,3,4,5};
+        // System.out.println(Arrays.stream(arr).sum());
     }
 }
